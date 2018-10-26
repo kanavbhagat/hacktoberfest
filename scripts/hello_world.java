51af9ba2f6c8222c20143af3dd1ce23491b54727
Jigme Lobsang lepcha <jiggylepcha@users.noreply.github.com>
2018-10-26 22:29:12 +0530

    Update hello_world.java

--- scripts/hello_world.java
+++ scripts/hello_world.java
@@ -9,5 +9,9 @@ public class hello_world {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+  
+    public static void checkHactober(){
+      System.out.println("Hey, I made this function for hacktober");
+    }
 
 }
